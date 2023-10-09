151ea972d5cd936b4aea94af204d2980f6181752
Krishelle <81546826+hbkris113@users.noreply.github.com>
2023-10-08 17:41:23 -0700

    Add files via upload
    My independent work!

---
+++ profile.js
@@ -0,0 +1,15 @@
+const color = document.getElementById("color");
+const place = document.getElementById("place");
+const ritual = document.getElementById("ritual");
+
+color.addEventListener("click", function () {
+  alert("My Favorite Color is Red!");
+});
+
+place.addEventListener("click", function () {
+  alert("My Favorite Place is Houston Texas");
+});
+
+ritual.addEventListener("click", function () {
+  alert("My Favorite Ritual is to eat red food on Juneteenth");
+});
